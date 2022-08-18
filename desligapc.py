1bc573c1e6874f33e02cc7bfdb46756108f4f76a
Joao_Vitor <joao.v.mpessoal@gmail.com>
2022-08-18 14:23:39 -0300

    first commit

---
+++ desligapc.py
@@ -0,0 +1,27 @@
+import subprocess
+from datetime import datetime
+
+data_e_hora_atuais = datetime.now()
+data_e_hora_em_texto = data_e_hora_atuais.strftime("%d/%m/%Y %H:%M")
+data_e_hora_em = data_e_hora_atuais.strftime("%d%m%Y%H%M")
+
+with open("config/pc.txt", "r") as tf:
+    lines = tf.read().split(',')
+
+with open("config/msg.txt", "r") as tf:
+    msg = tf.read()
+
+for line in lines:
+    Computer_List = [line]
+    for Computer in Computer_List:
+        a = subprocess.getoutput(
+            "shutdown /m \\\\" + Computer + " /f /s /c"+msg+"/t 60"+msg) + '\n'
+        arquivo = open('logs/log['+data_e_hora_em+'].txt', 'a')
+        arquivo.write(Computer + ' : ')
+        if len(a) > 2:
+            arquivo.write("Desligado  [" +
+                          data_e_hora_em_texto + "]\n")
+        else:
+            arquivo.write("Ok  [" +
+                          data_e_hora_em_texto + "]\n")
+        arquivo.close()
