c33bba2925914bcc7bf2dc0ca9fab07048613128
Joao_Vitor <joao.v.mpessoal@gmail.com>
2022-08-18 16:01:00 -0300

    ultimo commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 base = None
 
-executables = [Executable("desligapc.py", base=base)]
+executables = [Executable("desligapc.py", base=base,icon="icon.ico")]
 
 packages = ["idna"]
 options = {
