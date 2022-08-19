f30756bd32d41bfdb6c52d07736785a4216032b9
João Vitor Moraes <58138093+joaovrmoraes@users.noreply.github.com>
2022-08-19 11:40:40 -0300

    Add files via upload

--- desligapc.py
+++ desligapc.py
@@ -6,7 +6,7 @@ data_e_hora_em_texto = data_e_hora_atuais.strftime("%d/%m/%Y %H:%M")
 data_e_hora_em = data_e_hora_atuais.strftime("%d%m%Y%H%M")
 
 with open("config/pc.txt", "r") as tf:
-    lines = tf.read().split(',')
+    lines = tf.read().split('\n')
 
 with open("config/msg.txt", "r") as tf:
     msg = tf.read()
@@ -16,13 +16,15 @@ for line in lines:
     for Computer in Computer_List:
         print("Desligando o PC:" + Computer)
         a = subprocess.getoutput(
-            "shutdown /m \\\\" + Computer + " /f /s /c"+msg+"/t 60"+msg) + '\n'
+            "shutdown /m \\\\" + Computer + " /f /s /c"+msg+"/t 60"+msg)
         arquivo = open('logs/log['+data_e_hora_em+'].txt', 'a')
         arquivo.write(Computer + ' : ')
         if len(a) > 2:
+            print("PC Desligado")
             arquivo.write("Desligado  [" +
                           data_e_hora_em_texto + "]\n")
         else:
+            print("Desligando")
             arquivo.write("Ok  [" +
                           data_e_hora_em_texto + "]\n")
         arquivo.close()
